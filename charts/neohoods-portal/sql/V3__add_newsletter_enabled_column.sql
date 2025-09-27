7b4e8d2a123b10cab462785a21daae0ba748478c
Quentin Castel <quentin.castel@dataiku.com>
2025-09-27 21:54:04 +0200

    neohoods dev fake data

--- charts/neohoods-portal/sql/V3__add_newsletter_enabled_column.sql
+++
@@ -1,8 +0,0 @@
--- Add newsletter_enabled column to notification_settings table
-ALTER TABLE notification_settings 
-ADD COLUMN newsletter_enabled boolean NOT NULL DEFAULT true;
-
--- Update existing records to have newsletter_enabled = true by default
-UPDATE notification_settings 
-SET newsletter_enabled = true 
-WHERE newsletter_enabled IS NULL;
