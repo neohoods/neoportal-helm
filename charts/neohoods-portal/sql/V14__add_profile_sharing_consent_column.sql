4d8b81f0c103d0046729e94bf8823a5c62e62780
Quentin Castel <quentin.castel@dataiku.com>
2025-10-02 10:10:40 +0200

    default to true

--- charts/neohoods-portal/sql/V14__add_profile_sharing_consent_column.sql
+++ charts/neohoods-portal/sql/V14__add_profile_sharing_consent_column.sql
@@ -1,8 +1,8 @@
 -- Add profile_sharing_consent column to users table
 ALTER TABLE users 
-ADD COLUMN profile_sharing_consent boolean NOT NULL DEFAULT false;
+ADD COLUMN profile_sharing_consent boolean NOT NULL DEFAULT true;
 
 -- Update existing records to have profile_sharing_consent = false by default
 UPDATE users 
-SET profile_sharing_consent = false 
+SET profile_sharing_consent = true 
 WHERE profile_sharing_consent IS NULL;
